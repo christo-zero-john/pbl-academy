e66b6945d78fe888b754be0bf2b35a1cebf7a568
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-13 22:38:40 +0530

    Found modified or deleted file,  app/auth/check-login.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/check-login.jsx
+++ app/auth/check-login.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { User } from "@/modules/entities/User";
+import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 function CheckLogin({ children }) {
@@ -8,6 +9,9 @@ function CheckLogin({ children }) {
     async function execPromise() {
       let loginStatus = await User.checkLogin();
       console.log("login status", loginStatus);
+      if (loginStatus.status == 1) {
+        redirect("/dashboard");
+      }
     }
     execPromise();
   }, []);

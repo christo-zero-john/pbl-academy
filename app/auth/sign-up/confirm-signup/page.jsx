f63c0e16480ebd297b82466b4c0c181a69959e20
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-13 22:38:42 +0530

    Found new file,  app/auth/sign-up/confirm-signup/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/auth/sign-up/confirm-signup/page.jsx
@@ -0,0 +1,6 @@
+function ConfirmSignupPage() {
+  console.log("Checking and confirming create account");
+  return <div>Checking and confirming create account</div>;
+}
+
+export default ConfirmSignupPage;

4c422ed770bfcd24effb7648ff0b123da4d4eb6d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-13 23:37:49 +0530

    Found modified or deleted file,  app/auth/sign-up/confirm-signup/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/sign-up/confirm-signup/page.jsx
+++ app/auth/sign-up/confirm-signup/page.jsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { User } from "@/modules/entities/User";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
 function ConfirmSignupPage() {
-  console.log("Checking and confirming create account");
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const email = decodeURIComponent(searchParams.get("email"));
+    const password = decodeURIComponent(searchParams.get("password"));
+
+    async function execPromise() {
+      const { data, error } = await User.login(email, password);
+      if (!error) {
+        console.log("Logged in successfully...");
+        const loginStatus = await User.checkLogin();
+        console.log(loginStatus);
+
+        setTimeout(async () => {
+          const { data, error } = await User.saveUser();
+          if (error) {
+            console.log("Error confirming sign up: ", error);
+
+            const errorMessage =
+              error.message || "An error occurred while confirming sign up";
+            console.log(error.message);
+            window.confirm(errorMessage);
+          } else {
+            console.log(
+              "Account Created and confirmed Successfully. Redirecting to dashboard..."
+            );
+            redirect("/dashboard");
+          }
+        }, 5000);
+      }
+    }
+
+    execPromise();
+    console.log("Checking and confirming create account");
+  }, []);
+
   return <div>Checking and confirming create account</div>;
 }
 

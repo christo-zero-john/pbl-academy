b5f6bf19579c7cd5dcf093d049f422eb816ced42
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-14 20:58:52 +0530

    Found modified or deleted file,  app/auth/layout.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -10,7 +10,8 @@ export default function AuthenticationLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <CheckLogin>{children}</CheckLogin>
+        {children}
+        {/* <CheckLogin></CheckLogin> */}
       </body>
     </html>
   );

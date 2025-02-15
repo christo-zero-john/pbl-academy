5a9bc071f83bdc379b103a52ad54b46e783fa81f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-15 22:30:47 +0530

    Found modified or deleted file,  app/admin/dashboard/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function AdminPage() {
-  return <div>AdminPage</div>;
+  return (
+    <div>
+      <Link href="/admin/manage/users">Manage Users</Link>
+      <Link href="/admin/manage/mentors">Manage Mentors</Link>
+      <Link href="/admin/manage/learners">Manage Learners</Link>
+      <Link href="/admin/manage/courses">Manage Courses</Link>
+      <Link href="/admin/manage/programmes">Manage Programmes</Link>
+    </div>
+  );
 }

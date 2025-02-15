d186f5af793941842581921cca461c87681b1618
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-15 22:04:09 +0530

    Found new file,  app/admin/checkAdmin.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/admin/checkAdmin.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Admin } from "@/modules/entities/Admin";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function CheckAdmin({ children }) {
+  const [isAdmin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    async function execPromise() {
+      const checkAdminStatus = await Admin.checkAdmin();
+      if (checkAdminStatus.status == 0) {
+        redirect("/auth/login/");
+      } else if (checkAdminStatus.status == 1) {
+        setAdmin(true);
+      } else if (checkAdminStatus.status == 2) {
+        redirect("/dashboard");
+      }
+    }
+
+    execPromise();
+  }, []);
+
+  useEffect(() => {
+    console.log(isAdmin);
+  }, [isAdmin]);
+
+  return !isAdmin ? <h1>Entry Prohibited</h1> : <>{children}</>;
+}

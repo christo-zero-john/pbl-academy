3e0ba484fa61acee170b693dfe3609c85df4df39
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-15 18:09:29 +0530

    Found modified or deleted file,  app/auth/check-login.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/check-login.jsx
+++ app/auth/check-login.jsx
@@ -2,6 +2,7 @@
  * We must check whether a user is logged in or not to avoid logging in of multiple times.
  * If a user is logged in, then there is no need for them to login again.
  * The system should redirect them to their dashboard.
+ * This is a component only local for authentication check login
  */
 
 "use client";
@@ -14,7 +15,6 @@ function CheckLogin({ children }) {
   useEffect(() => {
     async function execPromise() {
       let loginStatus = await User.checkLogin();
-      console.log("login status", loginStatus);
       if (loginStatus.status == 1) {
         redirect("/dashboard");
       }

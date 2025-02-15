39f9c397b5d21615eedf06cac923eca607119e46
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-15 22:30:48 +0530

    Found new file,  app/admin/manage/users/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/admin/manage/users/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Admin } from "@/modules/entities/Admin";
+import { useEffect, useState } from "react";
+
+export default function ManageUsersPage() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const getUsersStatus = await Admin.getAllUsers();
+      if (getUsersStatus.status == 0) {
+        console.log("Error fetching user data: ", getUsersStatus.error);
+      } else {
+        setUsers(getUsersStatus.data);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+  return <div>ManageUsersPage</div>;
+}

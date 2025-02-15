a8736668201d1218e54eae8d6613ccddcf698712
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-15 20:16:08 +0530

    Found modified or deleted file,  app/dashboard/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,23 +4,42 @@
 
 "use client";
 import { User } from "@/modules/entities/User";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function UserDashboard() {
+  const [user, setUser] = useState({
+    email: "email",
+    email_verified: false,
+    first_name: "first",
+    last_name: "last",
+    phone_verified: false,
+    role: "",
+    sub: "40c8bed7-f923-47df-8934-afa249fb9388",
+  });
+
   useEffect(() => {
     async function execPromise() {
       let loginStatus = await User.checkAuth();
       console.log("Login Status: ", loginStatus);
-      const saveUserToSessionStatus = await User.saveUserToSession();
-      console.log(saveUserToSessionStatus);
+      if (loginStatus.status == 1) {
+        setUser(loginStatus.user.user_metadata);
+      }
     }
     execPromise();
   }, []);
 
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <div>
-      <h1 className="text-center">Welcome to PBL Academy</h1>
+      <h1 className="text-center">User Dashboard</h1>
+      <p className="fs-3">Welcome to PBL Academy</p>
       <p className="">Enroll in some course to start learning</p>
+      <div className="profile">
+        <h2>Profile</h2>
+      </div>
     </div>
   );
 }

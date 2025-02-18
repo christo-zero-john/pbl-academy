d606dbb16094f2625868ceb5d8152c419464e2a3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-18 22:44:13 +0530

    Found modified or deleted file,  modules/entities/Log.js. Committing Via Global Bash Script:git-status-automated.bat

--- modules/entities/Log.js
+++
@@ -1,9 +0,0 @@
-// Log is a global object that can be used to track logs during different operations
-export class Log {
-  constructor() {
-    this.message = [];
-  }
-  push(message) {
-    this.message.push(message);
-  }
-}

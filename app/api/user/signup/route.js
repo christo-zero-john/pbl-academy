1e575197202c8a15464bf74caac73f5d1d84d3b9
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-19 21:04:08 +0530

    Found modified or deleted file,  app/api/user/signup/route.js. Committing Via Global Bash Script:git-status-automated.bat

--- app/api/user/signup/route.js
+++ app/api/user/signup/route.js
@@ -1,10 +1,39 @@
 export async function POST(req) {
   try {
-    const body = await req.json(); 
+    const body = await req.json();
     const { email, password } = body;
 
-    return Response.json({ message: "User signed up!", email });
+    console.log(
+      `Start Creating new user account with email:${email} and password: ${password}`
+    );
+
+    const { data, error } = await Supabase.auth.signUp({ email, password });
+
+    if (error) {
+      // Invalid data sent to supabase (weak password, user already signed up etc...). 400: Bad Request
+      return Response.json(
+        { success: false, error: error.code },
+        { status: 400 }
+      );
+    } else {
+      // Successfully created user. 201: Success
+      return Response.json(
+        {
+          success: true,
+          message: "User signed up successfully!",
+          user: data.user,
+        },
+        { status: 201 }
+      );
+    }
   } catch (error) {
-    return Response.json({ error: "Something went wrong" }, { status: 500 });
+    // Soem unexpected error occurred. 500: Internal Server Error
+    return Response.json(
+      {
+        error:
+          "Something went wrong. Internal Server Error. Please Contact Support",
+      },
+      { status: 500 }
+    );
   }
 }

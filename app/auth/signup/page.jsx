94686bf37a03a9cb31cfbd98d65f621664a33a19
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-19 20:33:02 +0530

    Found modified or deleted file,  app/auth/signup/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,5 +1,65 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function SignupPage() {
-  return <div>SignupPage</div>;
+export default function SignUp() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  async function signuphandler(event) {
+    event.preventDefault();
+    console.log(formData);
+
+    let request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+
+    fetch("/api/user/signup", request)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  return (
+    <div>
+      <form onSubmit={signuphandler} className="">
+        <h1 className="">Create Account</h1>
+        <label className="">
+          <input
+            type="email"
+            className=""
+            name="email"
+            onChange={(event) =>
+              setFormData({ ...formData, email: event.target.value })
+            }
+            required
+            placeholder="Enter Email"
+          />
+        </label>
+        <label className="">
+          <input
+            type="text"
+            className=""
+            name="password"
+            required
+            minLength={8}
+            onChange={(event) =>
+              setFormData({ ...formData, password: event.target.value })
+            }
+            placeholder="Enter Password"
+          />
+        </label>
+        <button className="" type="submit">
+          Create Account
+        </button>
+        <p className="">
+          Already have an account?
+          <Link href="/auth/login">Login</Link>
+        </p>
+      </form>
+    </div>
+  );
 }

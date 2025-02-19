8ffe63b4c717d52f7d7fcb90f052f1d6687d9f63
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-19 22:19:11 +0530

    Found modified or deleted file,  app/auth/login/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,16 +1,48 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function LoginToAccount() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  function loginHandler(event) {
+    event.preventDefault();
+    const email = formData.email;
+    const password = formData.password;
+    console.log(formData);
+
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+
+    fetch("/api/auth/login", request)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
   return (
     <div>
-      <form className="">
+      <form onSubmit={loginHandler} className="">
         <p className="">Welcome Back</p>
         <h1>Login</h1>
         <label className="">
           <input
-            type="text"
+            type="email"
             className=""
             name="email"
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                email: event.target.value,
+              })
+            }
+            required
             placeholder="Enter Email"
           />
         </label>
@@ -19,6 +51,14 @@ export default function LoginToAccount() {
             type="text"
             className=""
             name="password"
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                password: event.target.value,
+              })
+            }
+            required
+            minLength={8}
             placeholder="Enter Password"
           />
         </label>

3617fda84d3dd13ed4908e79143a9e0d9671b7ce
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-19 23:32:58 +0530

    Found new file,  frontend/modules/entities/User.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ frontend/modules/entities/User.js
@@ -0,0 +1,55 @@
+import Supabase from "@/app/api/modules/entities/Supabase";
+
+class User {
+  constructor() {}
+
+  async setSession(session) {
+    try {
+      const { data, error } = await Supabase.auth.setSession(session);
+      if (error) {
+        return { success: false, error: error };
+      }
+      return { success: true, session: data };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+
+  async getSession() {
+    try {
+      const { data, error } = await Supabase.auth.getSession(session);
+      if (error) {
+        return { success: false, error: error };
+      }
+      return { success: true, session: data };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+
+  async getUser() {
+    try {
+      const { data, error } = await Supabase.auth.getUser();
+      if (error) {
+        return { success: false, error: error };
+      }
+      return { success: true, user: data };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+
+  async logout() {
+    try {
+      const { data, error } = await Supabase.auth.signOut();
+      if (error) {
+        return { success: false, error: error };
+      }
+      return { success: true, data: data };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+}
+
+export default new User();

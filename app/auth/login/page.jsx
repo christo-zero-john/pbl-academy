49e92a97dcf17eb15fcff39fb8703d1772936de6
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-19 20:33:01 +0530

    Found modified or deleted file,  app/auth/login/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import Link from "next/link";
 
-export default function LoginPage() {
-  return <div>LoginPage</div>;
+export default function LoginToAccount() {
+  return (
+    <div>
+      <form className="">
+        <p className="">Welcome Back</p>
+        <h1>Login</h1>
+        <label className="">
+          <input
+            type="text"
+            className=""
+            name="email"
+            placeholder="Enter Email"
+          />
+        </label>
+        <label className="">
+          <input
+            type="text"
+            className=""
+            name="password"
+            placeholder="Enter Password"
+          />
+        </label>
+        <button className="" type="submit">
+          Login
+        </button>
+        <p className="">
+          Dont have an account?
+          <Link href="/auth/signup">Create Account</Link>
+        </p>
+      </form>
+    </div>
+  );
 }

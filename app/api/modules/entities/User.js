612d13a6565f42c7d1eff8169a45af9f52f0ddb3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-20 07:28:52 +0530

    Found modified or deleted file,  app/api/modules/entities/User.js. Committing Via Global Bash Script:git-status-automated.bat

--- app/api/modules/entities/User.js
+++ app/api/modules/entities/User.js
@@ -1,5 +1,7 @@
 import Supabase from "./Supabase";
 
+// User in backend
+
 class User {
   constructor() {}
 
@@ -10,12 +12,25 @@ class User {
   }
 
   async login(email, password) {
-    const { data, error } = await Supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
+    try {
+      const { data, error } = await Supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
 
-    return { data, error };
+      if (!error) {
+        const userData = await this.getUserData(data.user.id);
+      }
+
+      return { data, error };
+    } catch (error) {
+      console.log("Internal Server Error: ", error);
+      throw new Error(error.message);
+    }
+  }
+
+  async getUserData(userId) {
+    console.log("Fetching from database, User data of user with Id: ", id);
   }
 }
 

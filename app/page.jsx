957c77d394bd6f68c1b8418537909e9315a29731
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-21 20:58:59 +0530

    Found modified or deleted file,  app/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,7 @@ import User from "@/frontend/modules/entities/User";
 import Link from "next/link";
 
 export default function HomePage() {
-  useEffect(() => {
-    console.log(User.getUser());
-  });
+
   return (
     <>
       <Link href="/auth/login">Login / Signup</Link> <br />

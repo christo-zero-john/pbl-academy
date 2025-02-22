29396f875958f07f248d8d308e3cdbaf3c526bd1
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-22 23:30:26 +0530

    Found modified or deleted file,  app/api/mentor/courses/create/route.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/api/mentor/courses/create/route.js
@@ -0,0 +1,47 @@
+import Mentor from "@/app/api/modules/entities/Mentor";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const courseData = await request.json();
+    console.log("Recieved Data: ", courseData);
+
+    console.log(
+      "Initializing create new course for user: ",
+      courseData.created_by
+    );
+
+    const createCourseStatus = await Mentor.createCourse(courseData);
+
+    if (createCourseStatus.success) {
+      console.log("Successfully created new course");
+      return NextResponse.json(
+        {
+          success: true,
+          data: createCourseStatus.data,
+        },
+        { status: 200 }
+      );
+    } else {
+      console.log("Error while creating course");
+      return NextResponse.json(
+        {
+          success: false,
+          error: createCourseStatus.error,
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.log("Internal Server Error: ", error);
+    return NextResponse.json(
+      {
+        success: false,
+        error:
+          `Internal Server Error: ${error.message}` ||
+          "Something went wrong. Internal Server Error. Please Contact Support",
+      },
+      { status: 500 }
+    );
+  }
+}

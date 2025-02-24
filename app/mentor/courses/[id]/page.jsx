b547cf19640b564da8e7fe94fdd4f586fa1fa880
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-24 22:04:35 +0530

    Found modified or deleted file,  app/mentor/courses/[id]/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/mentor/courses/[id]/page.jsx
+++ app/mentor/courses/[id]/page.jsx
@@ -1,19 +1,38 @@
 "use client";
 
-import Mentor from "@/frontend/modules/entities/Mentor";
+import Course from "@/frontend/modules/entities/Course";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+/**
+ *
+ * @returns Display a particular course
+ */
 export default function CourseItemPage() {
   const params = useParams();
   const [course, setCourse] = useState(null);
 
+  // This useEffect fetches the course and sets course item (course state)
   useEffect(() => {
+    /**
+     *  The below is a Immediately Invoked Function Expression which is an async function to fetch course from supabase. It is used as we can't directly use await statement inside  useEffect.
+     */
+
     (async () => {
-      const getCourseStatus = await Mentor.fetchCourse(params.id);
+      console.log("Fetching getCourseStatus");
+      const getCourseStatus = await Course.fetchCourses({
+        id: params.id,
+      });
 
       if (getCourseStatus.success) {
-        setCourse(getCourseStatus.course);
+        if (getCourseStatus.courses.length > 1) {
+          window.confirm(
+            "Something unexpected happened while fetching course. Instead of one, we got multiple courses!!!"
+          );
+        } else {
+          setCourse(getCourseStatus.courses[0]);
+          console.log(getCourseStatus.courses[0]);
+        }
       }
     })();
   }, []);
@@ -31,24 +50,17 @@ export default function CourseItemPage() {
           dangerouslySetInnerHTML={{ __html: course.description }}
         />
       </div>
-      <div>{course.created_by}</div>
+      <div>
+        <p className="">
+          Created By: &nbsp;
+          {course.created_by.first_name} {course.created_by.last_name}
+        </p>
+      </div>
       <div>{course.created_at}</div>
       <div>
-        <label htmlFor="published" className="">
-          Published
-          <input
-            id="published"
-            type="checkbox"
-            name=""
-            checked={course.published}
-            onChange={(event) =>
-              setCourse({
-                ...course,
-                published: event.target.checked,
-              })
-            }
-          />
-        </label>
+        <p className="fw-bold">
+          {(course.published && "Published") || "In Drafts"}
+        </p>
       </div>
       <div></div>
     </div>

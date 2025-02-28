8aa16e98fdd569b7efbc87a2ac681c197b2c7579
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-28 22:41:01 +0530

    Found new file,  app/auth/check-login.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/auth/check-login.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Supabase from "@/frontend/modules/entities/Supabase";
+import { redirect } from "next/navigation";
+
+function CheckLogin({ children }) {
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await Supabase.auth.getSession();
+      if (data?.session?.user) {
+        setIsloggedIn(true);
+      }
+    })();
+  }, []);
+
+  if (isLoggedIn) {
+    redirect("/dashboard");
+    return (
+      <>
+        <p className="">
+          You are already logged in.{" "}
+          <Link href="/dashboard">Go to Dashboard</Link>
+        </p>
+      </>
+    );
+  }
+
+  return <>{children}</>;
+}
+
+export default CheckLogin;

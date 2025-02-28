b779d0955934cb32f6b819fa3f128877616fcd28
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-28 21:50:11 +0530

    Found new file,  app/layout.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+/**
+ * Layout for the entire application, or Root Layout.
+ */
+
+import Bootstrap from "@/frontend/components/Bootstrap";
+import "@/frontend/styles/defaults.css";
+import { revalidatePath } from "next/cache";
+
+export const metadata = {
+  title: "PBL Academy",
+  description:
+    "The Worlds First Proof based learning Platform where you learn things by doing things and getting Hands-On Experience.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Bootstrap />
+      <body>{children}</body>
+    </html>
+  );
+}

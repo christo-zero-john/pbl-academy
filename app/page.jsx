e6af362656b3db6e27264546edbe9e01c91f791e
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-28 18:50:00 +0530

    Found modified or deleted file,  app/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,48 @@
-import User from "@/frontend/modules/entities/User";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <>
-      <Link href="/auth/login">Login / Signup</Link> <br />
-      <Link href="/dashboard">User Dashboard</Link>
+      <h1 className="text-center text-primary fs-3">All Paths in the App</h1>
+      <h2 className="text-center text-success fs-4">Authentication</h2>
+      <Link href="/auth/login" className="d-block m-1 btn btn-primary w-fit">
+        Login
+      </Link>{" "}
       <br />
-      <Link href="/mentor/courses/create">Create Course</Link>
+      <Link className="d-block m-1" href="/auth/signup">
+        Signup
+      </Link>{" "}
+      <br />
+      <Link className="d-block m-1 btn btn-primary w-fit" href="/dashboard">
+        User Dashboard
+      </Link>
+      <br />
+      <hr className="border border-3" />
+      <h2 className="text-center text-success fs-4">Courses</h2>
+      <Link className="d-block m-1" href="/courses">
+        All Courses
+      </Link>
+      <br />
+      <Link className="d-block m-1" href="/courses/course-id">
+        Display Course By ID
+      </Link>
+      <br />
+      <Link className="d-block m-1" href="/courses/404">
+        Course Not Found
+      </Link>
+      <br />
+      <Link className="d-block m-1" href="/courses/mentor/">
+        Mentor Dashboard
+      </Link>
+      <br />
+      <Link className="d-block m-1" href="/courses/mentor/all-courses">
+        All Courses of a Mentor
+      </Link>
+      <br />
+      <Link className="d-block m-1" href="/courses/mentor/create-course">
+        Create New Course
+      </Link>
+      <br />
     </>
   );
 }

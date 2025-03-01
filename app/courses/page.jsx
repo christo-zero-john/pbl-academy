091bfe051abb6ba5592ac0366e42958b3f1d7fe5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-01 21:17:02 +0530

    Found modified or deleted file,  app/courses/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,3 +1,61 @@
-export default function CoursesPage() {
-  return <div>All Courses Page</div>;
+"use client";
+
+import Course from "@/frontend/modules/entities/Course";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+function AllCourses() {
+  const [courses, setCourses] = useState(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      console.log("Fetching getCourseStatus");
+      const getCourseStatus = await Course.fetchCourses();
+
+      console.log(getCourseStatus);
+
+      if (getCourseStatus.success) {
+        console.log("Successfully fetched courses");
+        setCourses(getCourseStatus.courses);
+      } else {
+        window.confirm("Error Fetching Course...");
+      }
+    })();
+  }, []);
+
+  function viewCourseHandler(event) {
+    router.push(`/courses/${event.target.id}`);
+  }
+
+  if (courses == null) {
+    return <div>Fetching Courses...</div>;
+  } else {
+    if (courses.length == 0) {
+      return <div>No Courses Found</div>;
+    } else {
+      return (
+        <div>
+          {courses.map((course) => (
+            <div
+              key={course.id}
+              className="border border-3 border-secondary p-3 m-2 w-fit"
+            >
+              <h1 className="fs-5 fw- d-inline-block mx-2">{course.title}</h1>
+              <button
+                className="btn btn-primary d-inline-block mx-2"
+                onClick={viewCourseHandler}
+                id={course.id}
+              >
+                View
+              </button>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
 }
+
+export default AllCourses;

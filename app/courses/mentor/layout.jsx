b30202199695117727786e762d56d6ace499759c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-01 20:17:16 +0530

    Found new file,  app/courses/mentor/layout.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/courses/mentor/layout.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import User from "@/frontend/modules/entities/User";
+
+export default function MenotorLayout({ children }) {
+  // Render child pages only if the user is a mentor. Else restrict access
+  if (User.user.user_metadata.role.includes("mentor")) {
+    return <div>{children}</div>;
+  } else {
+    return (
+      <>
+        <h1 className="text-center m-5 pt-5 text-danger ">
+          Sorry Access Restricted!! Only Accessible for mentors. Request to be a mentor
+          to see this page....
+        </h1>
+      </>
+    );
+  }
+}

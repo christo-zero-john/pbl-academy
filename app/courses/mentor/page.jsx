a14bb72a9270dfd527d2bb3e5c42fb524dba3d23
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-01 20:17:16 +0530

    Found new file,  app/courses/mentor/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/courses/mentor/page.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function MentorDasboard() {
+  return (
+    <div>
+      <Link href="/courses/mentor/all-courses">All Courses</Link>
+    </div>
+  );
+}

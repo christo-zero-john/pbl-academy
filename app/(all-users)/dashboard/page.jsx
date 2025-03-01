3bae6d4ca2c13fcc069ac12ae147edced608b8d2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-01 14:48:08 +0530

    Found new file,  app/(all-users)/dashboard/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/(all-users)/dashboard/page.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import User from "@/frontend/modules/entities/User";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function UserDashboard() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    console.log(User.user);
+    setUser(User.user);
+  }, []);
+
+  async function logoutHandler() {
+    const response = await User.logout();
+    console.log(response);
+    if (response.success) {
+      window.confirm("Logged Out successfully");
+      redirect("/auth/login");
+    } else {
+      window.confirm(response.error);
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="">
+        Welcome,{" "}
+        <span className="">
+          {user?.user_metadata.first_name || "First Name"}
+        </span>
+        &nbsp;
+        <span className="">{user?.user_metadata.last_name || "Last Name"}</span>
+      </h1>
+      <button className="" onClick={logoutHandler}>
+        Logout
+      </button>
+
+      {user?.user_metadata.role.includes("mentor") ? (
+        <button className="">
+          <a href="/mentor" className="">
+            Mentor Dashboard
+          </a>
+        </button>
+      ) : (
+        "Not Mentor"
+      )}
+    </div>
+  );
+}

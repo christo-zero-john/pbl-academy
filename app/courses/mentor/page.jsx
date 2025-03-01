7617867b66deaecd970a28e72cba70346d60df32
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-01 20:45:22 +0530

    Found modified or deleted file,  app/courses/mentor/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/mentor/page.jsx
+++ app/courses/mentor/page.jsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 export default function MentorDasboard() {
   return (
     <div>
-      <Link href="/courses/mentor/all-courses">All Courses</Link>
+      <Link href="/courses/mentor/all-courses">All Courses</Link> <br />
+      <Link href="/courses/mentor/create-course">New Course</Link>
     </div>
   );
 }

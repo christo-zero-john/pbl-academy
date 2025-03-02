630238260926c49822b05b054da1c776fd85cab3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-02 20:44:25 +0530

    Found modified or deleted file,  app/courses/mentor/edit-course/[id]/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/mentor/edit-course/[id]/page.jsx
+++ app/courses/mentor/edit-course/[id]/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
+import TextEditor from "@/frontend/components/text-editor/text-editor";
 import Course from "@/frontend/modules/entities/Course";
+import Mentor from "@/frontend/modules/entities/Mentor";
 import User from "@/frontend/modules/entities/User";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -18,9 +20,9 @@ export default function EditCoursePage() {
     description: course?.description,
   });
 
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
+  // useEffect(() => {
+  //   console.log("Form Data: ", formData);
+  // }, [formData]);
 
   // This useEffect fetches the course and sets course item (course state)
   useEffect(() => {
@@ -44,7 +46,8 @@ export default function EditCoursePage() {
           );
         } else {
           setCourse(getCourseStatus.courses[0]);
-          console.log(getCourseStatus.courses[0]);
+          setFormData(getCourseStatus.courses[0]);
+          // console.log(getCourseStatus.courses[0]);
         }
       } else {
         router.push("/courses/404");
@@ -56,30 +59,94 @@ export default function EditCoursePage() {
     return <div>Fetching Course...</div>;
   }
 
+  if (course && course.created_by.id != User?.user.id) {
+    return (
+      <div className="">
+        <p className="fs-3 fw-bold text-danger">
+          You are not allowed to perform this operation for this course. Request
+          access to be a mentor to be able to perform this operation...
+        </p>
+      </div>
+    );
+  }
+
+  function setDescription(description) {
+    setFormData({
+      ...formData,
+      description: description,
+    });
+  }
+
+  async function updateCourseDetailsHandler(event) {
+    event.preventDefault();
+    console.log("Upadting Details of course with: ", formData);
+    setFormData({
+      ...formData,
+      created_by: formData.created_by.id,
+    });
+    const updateCourseDeatilsStatus = await Mentor.updateCourse({
+      ...formData,
+    }); // using spread operator, otherwise only a reference is sent to the function, which will cause errors
+  }
+
+  if (!course) {
+    return <div className="loading-text">Fetching Course...</div>;
+  }
+
   return (
     <div className="">
-      <div className="header">
-        <h1 className="fs-3 text-center">Edit Course: {course.title}</h1>
-      </div>
-      <div>
-        <p
-          className=""
-          dangerouslySetInnerHTML={{ __html: course.description }}
-        />
-      </div>
-      <div>
-        <p className="">
-          Created By: &nbsp;
-          {course.created_by.first_name} {course.created_by.last_name}
-        </p>
-      </div>
-      <div>{course.created_at}</div>
-      <div>
-        <p className="fw-bold">
-          {(course.published && "Published") || "In Drafts"}
-        </p>
-      </div>
-      <div></div>
+      <h1 className="text-center">Edit Course Details</h1>
+
+      <form className="p-3" onSubmit={updateCourseDetailsHandler}>
+        <label htmlFor="published" className="">
+          Publish
+          <input
+            type="checkbox"
+            name="published"
+            id="published"
+            className=""
+            onChange={(event) => {
+              setFormData({
+                ...formData,
+                published: event.target.checked,
+              });
+            }}
+          />
+        </label>
+        <label htmlFor="title" className="d-block">
+          Course Title
+          <input
+            type="text"
+            id="title"
+            className="d-block"
+            name="title"
+            required
+            minLength={3}
+            value={formData.title || ""}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                title: event.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label htmlFor="" className="d-block">
+          Course Description
+          <TextEditor
+            onChange={setDescription}
+            initialContent={formData.description || "Loading..."}
+          />
+        </label>
+
+        <button
+          className="d-block btn btn-success col-11 col-md-6 mx-auto my-2"
+          type="submit"
+        >
+          Update Course
+        </button>
+      </form>
     </div>
   );
 }

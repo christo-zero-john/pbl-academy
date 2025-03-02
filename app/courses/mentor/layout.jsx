a820ff51406a3cd1e30aaf60e39a970f0171db17
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-02 20:44:25 +0530

    Found modified or deleted file,  app/courses/mentor/layout.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/mentor/layout.jsx
+++ app/courses/mentor/layout.jsx
@@ -1,20 +1,14 @@
 "use client";
 
 import User from "@/frontend/modules/entities/User";
+import { CheckMentor } from "./check-mentor";
 
 export default function MenotorLayout({ children }) {
   // Render child pages only if the user is a mentor. Else restrict access
 
-  if (User.user.user_metadata.role.includes("mentor")) {
-    return <div>{children}</div>;
-  } else {
-    return (
-      <>
-        <h1 className="text-center m-5 pt-5 text-danger ">
-          Sorry Access Restricted!! Only Accessible for mentors. Request to be a
-          mentor to see this page....
-        </h1>
-      </>
-    );
-  }
+  return (
+    <>
+      <CheckMentor>{children}</CheckMentor>
+    </>
+  );
 }

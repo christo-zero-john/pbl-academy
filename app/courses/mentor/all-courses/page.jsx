d48a89814add34bc81f0b182051b653f98835e72
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-02 21:13:46 +0530

    Found modified or deleted file,  app/courses/mentor/all-courses/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/mentor/all-courses/page.jsx
+++ app/courses/mentor/all-courses/page.jsx
@@ -45,7 +45,7 @@ function AllCoursesOfMentor() {
           {courses.map((course) => (
             <div
               key={course.id}
-              className="border border-3 border-secondary p-3 m-2 w-fit"
+              className="border border-3 border-secondary p-3 m-2 w-fit d-inline-block m-2"
             >
               <div className="">
                 <h1 className="fs-5 fw- d-inline-block mx-2">{course.title}</h1>

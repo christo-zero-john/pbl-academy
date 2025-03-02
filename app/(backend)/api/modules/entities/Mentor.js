0c0fe50eddc016d9abb57b981d1e1f11d2b9771f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-02 20:44:24 +0530

    Found modified or deleted file,  app/(backend)/api/modules/entities/Mentor.js. Committing Via Global Bash Script:git-status-automated.bat

--- app/(backend)/api/modules/entities/Mentor.js
+++ app/(backend)/api/modules/entities/Mentor.js
@@ -13,26 +13,82 @@ class Mentor {
   }
 
   async createCourse(course) {
-    const Supabase = await createClient();
-    const userSession = await Supabase.auth.getSession();
-    console.log(userSession.data.session.user);
-    const { data, error } = await Supabase.from("courses")
-      .insert([
-        {
-          ...course,
-        },
-      ])
-      .select("*");
+    try {
+      const Supabase = await createClient();
+      const userSession = await Supabase.auth.getSession();
+      console.log(
+        "user session fetched successfully. Current User: ",
+        userSession.data.session.user.email
+      );
+      const { data, error } = await Supabase.from("courses")
+        .insert([
+          {
+            ...course,
+          },
+        ])
+        .select("*");
 
-    if (error) {
+      if (error) {
+        return {
+          success: false,
+          error: error,
+        };
+      } else {
+        return {
+          success: true,
+          data: data[0],
+        };
+      }
+    } catch (error) {
+      // Catch unexpected errors
+      console.log("Internal Server Error: ", error);
       return {
         success: false,
-        error: error,
+        error:
+          `Internal Server Error: ${error.message}` ||
+          "Something went wrong. Internal Server Error. Please Contact Support",
       };
-    } else {
+    }
+  }
+
+  async updateCourse(course) {
+    try {
+      const Supabase = await createClient();
+      const userSession = await Supabase.auth.getSession();
+      console.log(
+        "user session fetched successfully. Current User: ",
+        userSession.data.session.user.email
+      );
+      const courseId = course.id;
+      delete course.id;
+      console.log("Course Data: ", course, "Course ID: ", courseId);
+
+      const { data, error } = await Supabase.from("courses")
+        .update({
+          ...course,
+        })
+        .eq("id", courseId)
+        .select("*");
+
+      if (error) {
+        return {
+          success: false,
+          error: error,
+        };
+      } else {
+        return {
+          success: true,
+          data: data[0],
+        };
+      }
+    } catch (error) {
+      // Catch unexpected errors
+      console.log("Internal Server Error: ", error);
       return {
-        success: true,
-        data: data[0],
+        success: false,
+        error:
+          `Internal Server Error: ${error.message}` ||
+          "Something went wrong. Internal Server Error. Please Contact Support",
       };
     }
   }

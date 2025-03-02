fc0e9bcf93d377200d924693630772f1c71ba07a
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-02 20:44:27 +0530

    Found new file,  app/courses/mentor/check-mentor.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/courses/mentor/check-mentor.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import User from "@/frontend/modules/entities/User";
+
+export function CheckMentor({ children }) {
+  const [isMentor, setIsMentor] = useState(false);
+
+  useEffect(() => {
+    // console.log(User.user);
+    (async () => {
+      const user = await User.getUser();
+      console.log(typeof user);
+      if (user.user_metadata.role.includes("mentor")) {
+        setIsMentor(true);
+      }
+    })();
+  }, []);
+
+  if (!isMentor) {
+    return (
+      <>
+        <h1 className="text-center m-5 pt-5 text-danger ">
+          Sorry Access Restricted!! Only Accessible for mentors. Request to be a
+          mentor to see this page....
+        </h1>
+      </>
+    );
+  } else {
+    return <>{children}</>;
+  }
+}

a79cc55b843904c41a1522868bf0a45044c9714c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-03 22:46:02 +0530

    Found modified or deleted file,  app/courses/[id]/task-day-wise.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/[id]/task-day-wise.jsx
+++ app/courses/[id]/task-day-wise.jsx
@@ -1,5 +1,38 @@
-import React from "react";
-
 export default function TaskDayWise({ tasks, day }) {
-  return <div>TaskDayWise</div>;
+  return (
+    <>
+      <div
+        class="accordion accordion-flush"
+        id={"accordion-tasks-of-day-" + day}
+      >
+        {tasks.map((task, index) => (
+          <div class="accordion-item" key={index}>
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target={
+                  "#flush-collapse-day-" + day + "-task-" + (index + 1)
+                }
+                aria-expanded="false"
+                aria-controls={
+                  "flush-collapse-day-" + day + "-task-" + (index + 1)
+                }
+              >
+                Task {index + 1}
+              </button>
+            </h2>
+            <div
+              id={"flush-collapse-day-" + day + "-task-" + (index + 1)}
+              class="accordion-collapse collapse"
+              data-bs-parent={"#accordion-tasks-of-day-" + day}
+            >
+              <div class="accordion-body">{task.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }

322ff5bbd16f1407cbd9ee9cd4d846365d7b69ca
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-05 22:15:06 +0530

    Found modified or deleted file,  app/courses/mentor/edit-course/[id]/tasks/add-task-form.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/mentor/edit-course/[id]/tasks/add-task-form.jsx
+++ app/courses/mentor/edit-course/[id]/tasks/add-task-form.jsx
@@ -16,7 +16,7 @@ export default function AddTaskForm({
     day: day,
     index: null,
     created_by: User.user.id,
-    duration: [0, 0],
+    duration: 1,
     title: "",
   });
 
@@ -33,31 +33,45 @@ export default function AddTaskForm({
     if (task.description.length <= 100) {
       window.confirm("Task Description must contain atleast 100 letters.");
     } else {
-      submitHandler(task);
+      const submitSuccess = submitHandler({ ...task });
+      if (submitSuccess) {
+        console.log("Successfully created new task");
+        event.target.reset();
+        setTask({
+          course_id: "",
+          description: "",
+          day: day,
+          index: null,
+          created_by: User.user.id,
+          duration: 1,
+          title: "",
+        });
+      }
       setShow(false);
     }
   }
+
   return (
     <div
-      class={`offcanvas col-12 wd-100 fixed-top ${show && "show"}`}
+      className={`offcanvas col-12 wd-100 fixed-top ${show && "show"}`}
       data-bs-backdrop="static"
-      tabindex="-1"
+      tabIndex="-1"
       id="staticBackdrop"
       aria-labelledby="add-task-form"
     >
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="add-task-form">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="add-task-form">
           Add new task to day {day}
         </h5>
         <button
           type="button"
-          class="btn-close"
+          className="btn-close"
           data-bs-dismiss="offcanvas"
           aria-label="Close"
           onClick={() => setShow(false)}
         ></button>
       </div>
-      <div class="offcanvas-body">
+      <div className="offcanvas-body">
         <form method="POST" className="col-12" onSubmit={formSubmitHandler}>
           <input
             type="text"
@@ -76,6 +90,26 @@ export default function AddTaskForm({
           <label htmlFor="" className="d-block">
             Description
             <TextEditor initialContent="" onChange={setDescription} />
+          </label>
+          <label htmlFor="duration">
+            Task Duration{" "}
+            <p className="p-0 m-2">
+              <input
+                type="number"
+                min={1}
+                max={60}
+                className=""
+                required
+                placeholder="1 - 60"
+                onChange={(event) =>
+                  setTask({
+                    ...task,
+                    duration: +event.target.value, // + operator to cast string value to number
+                  })
+                }
+              />{" "}
+              minutes
+            </p>
           </label>
           <AddResourceLinksForm />
           <button type="submit">Add New Task</button>

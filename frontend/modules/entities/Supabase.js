64bb7becdcc7a77665036c1c238f6f0db5f7966b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-05 20:37:14 +0530

    Found modified or deleted file,  frontend/modules/entities/Supabase.js. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/modules/entities/Supabase.js
+++ frontend/modules/entities/Supabase.js
@@ -1,5 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+/**
+ * Used to create anew supabase client in the frontend/browser
+ */
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,

00dc149bba282625625d30df89a7d4eaad80c190
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-05 22:15:05 +0530

    Found modified or deleted file,  app/courses/[id]/task-day-wise.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/[id]/task-day-wise.jsx
+++ app/courses/[id]/task-day-wise.jsx
@@ -1,4 +1,4 @@
-export default function TaskDayWise({ tasks, day, addNewTaskHandler }) {
+export default function TaskDayWise({ tasks, day, addNewTaskHandler = null }) {
   // The below check is needed as tasks needed to be grouped, sorted and converted into a 2D array. If it is not a 2D array, it will cause error in the task.map method below in the return statement.
   if (!Array.isArray(tasks)) {
     return (
@@ -44,12 +44,20 @@ export default function TaskDayWise({ tasks, day, addNewTaskHandler }) {
           </div>
         ))}
       </div>
-      <button
-        className="border border-3 text-center py-3 col-12"
-        onClick={() => addNewTaskHandler(day)}
-      >
-        Add New Task
-      </button>
+      {
+        /**
+         * This component is used in edit-task and course pages. Only edit-task page @function addNewTaskhandler.
+         * If @function addNewTaskhandler is present it means this is an edit page. Then we can show the 'Add New Task' button.
+         */
+        addNewTaskHandler && (
+          <button
+            className="border border-3 text-center py-3 col-12"
+            onClick={() => addNewTaskHandler(day)}
+          >
+            Add New Task
+          </button>
+        )
+      }
     </>
   );
 }

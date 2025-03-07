fce66e5b186de9994e6a1ece9677da2bbb1884cd
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-07 22:52:23 +0530

    Update render-tasks.jsx

--- app/courses/mentor/edit-course/[id]/tasks/render-tasks.jsx
+++ app/courses/mentor/edit-course/[id]/tasks/render-tasks.jsx
@@ -4,26 +4,15 @@ import TaskDayWise from "@/app/courses/[id]/task-day-wise";
  * This componsnt is used in course-item and edit-tasks pages.
  * @var props with @default null value means thay are optional props passed only from edit-task page.
  * @param tasks It is a 2D array of tasks related to a course.
- * @param setShowTaskForm is used to handle a boolean state of whether to show or hide edit-task form in the edit-task page.
  */
-export default function RenderTasks({
-  tasks,
-  setShowTaskForm = null,
-  setDay = null,
-}) {
-  function addNewTaskHandler(day) {
-    console.log("Adding new task to day: ", day);
-    if (setDay && typeof setDay === 'function') {
-      setDay(day);
-      console.log("Day set to:", day);
-    }
-    if (setShowTaskForm && typeof setShowTaskForm === 'function') {
-      setShowTaskForm(true);
-    }
-  }
-
+export default function RenderTasks({ tasks, addNewTaskHandler = null }) {
+  console.log("Rendering Course tasks: ", tasks);
   if (tasks.length === 0) {
     return <p>No tasks added yet.</p>;
+  }
+
+  if (tasks.length === 1 && tasks[0].length === 0) {
+    return <p>No tasks found for existing day.</p>;
   }
 
   return (
@@ -41,7 +30,7 @@ export default function RenderTasks({
                   aria-expanded="false"
                   aria-controls={"flush-collapse-day-" + (index + 1)}
                 >
-                  Day {index + 1}
+                  Day {dayTasks[0]?.day || index + 1}
                 </button>
               </h2>
               <div
@@ -51,8 +40,8 @@ export default function RenderTasks({
               >
                 <div className="accordion-body">
                   <TaskDayWise
-                    tasks={[...dayTasks]}
-                    day={index + 1}
+                    tasks={dayTasks}
+                    day={dayTasks[0]?.day || index + 1}
                     addNewTaskHandler={addNewTaskHandler}
                   />
                 </div>

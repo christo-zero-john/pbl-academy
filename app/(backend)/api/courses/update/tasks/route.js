7defe8ba537fd2146910cb0da0f096f2ba0ee435
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-09 16:14:40 +0530

    Found new file,  app/(backend)/api/courses/update/tasks/route.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/(backend)/api/courses/update/tasks/route.js
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import Mentor from "../../../modules/entities/Mentor";
+
+/**
+ * API Route to add or update a task
+ */
+export async function POST(request) {
+  try {
+    const tasks = await request.json();
+    console.log("Recieved Tasks: ", tasks);
+
+    console.log("Initializing 'Save Tasks to database'");
+
+    const updateStatus = await Mentor.saveTasksToDB(tasks);
+
+    console.log(updateStatus);
+
+    if (updateStatus.success) {
+      console.log("Successfully updated course");
+      return NextResponse.json(
+        {
+          success: true,
+          data: updateStatus.data,
+        },
+        { status: 200 }
+      );
+    } else {
+      console.log("Error while updating course");
+      return NextResponse.json(
+        {
+          success: false,
+          error: updateStatus.error,
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.log("Internal Server Error: ", error);
+    return NextResponse.json(
+      {
+        success: false,
+        error:
+          `Internal Server Error: ${error.message}` ||
+          "Something went wrong. Internal Server Error. Please Contact Support",
+      },
+      { status: 500 }
+    );
+  }
+}

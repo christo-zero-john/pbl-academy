dd010ed742809f31de57748830f5c02ae6953b58
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-14 21:45:29 +0530

    Found modified or deleted file,  app/courses/mentor/all-courses/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/mentor/all-courses/page.jsx
+++ app/courses/mentor/all-courses/page.jsx
@@ -4,6 +4,7 @@ import Course from "@/frontend/modules/entities/Course";
 import User from "@/frontend/modules/entities/User";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import DisplayCourses from "./display-courses";
 
 function AllCoursesOfMentor() {
   const router = useRouter();
@@ -11,25 +12,21 @@ function AllCoursesOfMentor() {
 
   useEffect(() => {
     (async () => {
-      console.log("Fetching getCourseStatus");
+      console.log("Fetching Courses");
       const getCourseStatus = await Course.fetchCourses({
         created_by: User.user.id,
       });
 
-      console.log(getCourseStatus);
+      // console.log(getCourseStatus);
 
       if (getCourseStatus.success) {
         console.log("Successfully fetched courses");
         setCourses(getCourseStatus.courses);
       } else {
-        window.confirm("Error Fetching Course...");
+        window.confirm("Some unexpected Error while Fetching Course...");
       }
     })();
   }, []);
-
-  function viewCourseHandler(event) {
-    router.push(`/courses/${event.target.id}`);
-  }
 
   if (courses == null) {
     return <div>Fetching Courses...</div>;
@@ -37,34 +34,7 @@ function AllCoursesOfMentor() {
     if (courses.length == 0) {
       return <div>No Courses Found</div>;
     } else {
-      return (
-        <div>
-          <p className="">
-            Displaying {courses.length} courses of {User.user.id}
-          </p>
-          {courses.map((course) => (
-            <div
-              key={course.id}
-              className="border border-3 border-secondary p-3 m-2 w-fit d-inline-block m-2"
-            >
-              <div className="">
-                <h1 className="fs-5 fw- d-inline-block mx-2">{course.title}</h1>
-                <p className="small">
-                  <span className="fw-bold">By: </span>
-                  {course.created_by.id}
-                </p>
-              </div>
-              <button
-                className="btn btn-primary d-inline-block mx-2"
-                onClick={viewCourseHandler}
-                id={course.id}
-              >
-                View
-              </button>
-            </div>
-          ))}
-        </div>
-      );
+      return <DisplayCourses courses={courses} />;
     }
   }
 }

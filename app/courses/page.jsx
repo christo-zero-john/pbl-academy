2b4d7d6cf5a5e8b17fe41d7231dc3646cff0c932
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-14 21:45:29 +0530

    Found modified or deleted file,  app/courses/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Course from "@/frontend/modules/entities/Course";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import DisplayCourses from "./mentor/all-courses/display-courses";
 
 function AllCourses() {
   const [courses, setCourses] = useState(null);
@@ -11,10 +13,10 @@ function AllCourses() {
 
   useEffect(() => {
     (async () => {
-      console.log("Fetching getCourseStatus");
+      console.log("Fetching Courses");
       const getCourseStatus = await Course.fetchCourses();
 
-      console.log(getCourseStatus);
+      // console.log(getCourseStatus);
 
       if (getCourseStatus.success) {
         console.log("Successfully fetched courses");
@@ -25,42 +27,13 @@ function AllCourses() {
     })();
   }, []);
 
-  function viewCourseHandler(event) {
-    router.push(`/courses/${event.target.id}`);
-  }
-
   if (courses == null) {
     return <div>Fetching Courses...</div>;
   } else {
     if (courses.length == 0) {
       return <div>0 Published Courses Found</div>;
     } else {
-      return (
-        <div>
-          <p className="text-center">Displaying {courses.length} courses</p>
-          {courses.map((course) => (
-            <div
-              key={course.id}
-              className="border border-3 border-secondary p-3 m-2 w-fit d-inline-block m-2"
-            >
-              <div className="">
-                <h1 className="fs-5 fw- d-inline-block mx-2">{course.title}</h1>
-                <p className="small">
-                  <span className="fw-bold">By: </span>
-                  {course.created_by.id}
-                </p>
-              </div>
-              <button
-                className="btn btn-primary d-inline-block mx-2"
-                onClick={viewCourseHandler}
-                id={course.id}
-              >
-                View
-              </button>
-            </div>
-          ))}
-        </div>
-      );
+      return <DisplayCourses courses={courses} />;
     }
   }
 }

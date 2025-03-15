86f09f212dd405e0430a31ceec3bb8a7c2e048fb
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-15 19:09:45 +0530

    Found modified or deleted file,  frontend/components/classrooms/all-course-classrooms.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/classrooms/all-course-classrooms.jsx
+++ frontend/components/classrooms/all-course-classrooms.jsx
@@ -1,35 +1,32 @@
-export default function AllCourseClassrooms({ courseID, show }) {
+import { Offcanvas, Accordion } from "react-bootstrap";
+
+export default function AllCourseClassrooms({ courseID, show, setShow }) {
   return (
     <>
-      <div className="accordion accordion-flush" id="accordion-tasks-daywise">
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target={"#flush-collapse-day-" + "index + 1"}
-              aria-expanded="false"
-              aria-controls={"flush-collapse-day-" + "index + 1"}
-            >
-              Accordion 1
-            </button>
-          </h2>
-          <div
-            id={"flush-collapse-day-" + "index + 1"}
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordion-tasks-daywise"
-          >
-            <p className="">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum,
-              maxime! Animi sed laborum nihil harum quibusdam magnam cupiditate
-              officia nesciunt dolorem molestiae optio consectetur rem veritatis
-              quaerat adipisci debitis fuga sit blanditiis unde, expedita
-              accusantium?
-            </p>
-          </div>
-        </div>
-      </div>
+      <Offcanvas
+        show={show}
+        onHide={() => setShow(false)}
+        backdrop="static"
+        className="w-100"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>All Classrooms</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Accordion defaultActiveKey="null" flush>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Accordion 1</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum,
+                maxime! Animi sed laborum nihil harum quibusdam magnam
+                cupiditate officia nesciunt dolorem molestiae optio consectetur
+                rem veritatis quaerat adipisci debitis fuga sit blanditiis unde,
+                expedita accusantium?
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 }

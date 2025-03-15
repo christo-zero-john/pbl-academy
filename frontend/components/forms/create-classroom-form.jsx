00c2d7e25263498626d951cb986c799b22abd718
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-15 23:15:08 +0530

    Found modified or deleted file,  frontend/components/forms/create-classroom-form.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/forms/create-classroom-form.jsx
+++ frontend/components/forms/create-classroom-form.jsx
@@ -1,3 +1,37 @@
-export default function CreateClassroomForm() {
-  return <div>CreateClassroomForm</div>;
+import { useState } from "react";
+import { Offcanvas } from "react-bootstrap/esm";
+
+export default function CreateClassroomForm({ course, show, setShow }) {
+  const [formData, setFormData] = useState({});
+
+  async function createClassroomHandler(event) {
+    event.preventDefault();
+    console.log("Creating New Classroom for course: ", course.id);
+  }
+
+  return (
+    <Offcanvas
+      show={show}
+      onHide={() => setShow(false)}
+      backdrop="static"
+      className="w-100"
+    >
+      <Offcanvas.Header
+        closeButton
+        className="border-bottom border-3 border-warning"
+      >
+        <Offcanvas.Title>
+          <span className="fs-6 d-block">Create New Classroom for</span>{" "}
+          <span className="d-block fs-2 text-success">{course.title}</span>
+        </Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <form
+          method="POST"
+          className="col-12"
+          onSubmit={createClassroomHandler}
+        ></form>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
 }

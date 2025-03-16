a923b5f79d36fceccb76e2578c6e2cce951af1c5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-16 08:33:31 +0530

    Found new file,  app/(backend)/api/courses/classrooms/new/route.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/(backend)/api/courses/classrooms/new/route.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import Mentor from "../../../modules/entities/Mentor";
+
+/**
+ * API Route to create a new classroom
+ */
+export async function POST(request) {
+  try {
+    const classroomData = await request.json();
+    console.log("Recieved Data: ", classroomData);
+
+    console.log(
+      "Initializing create new classrrom for course: ",
+      classroomData.course_id
+    );
+
+    const createClassroomStatus = await Mentor.createClassroom(classroomData);
+
+    if (createClassroomStatus.success) {
+      console.log("Successfully created new classroom");
+      return NextResponse.json(
+        {
+          success: true,
+          data: createClassroomStatus.data,
+        },
+        { status: 200 }
+      );
+    } else {
+      console.log("Error while creating new classroom");
+      return NextResponse.json(
+        {
+          success: false,
+          error: createClassroomStatus.error,
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.log("Internal Server Error: ", error);
+    return NextResponse.json(
+      {
+        success: false,
+        error:
+          `Internal Server Error: ${error.message}` ||
+          "Something went wrong. Internal Server Error. Please Contact Support",
+      },
+      { status: 500 }
+    );
+  }
+}

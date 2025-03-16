81b83df577ec383ac1fbcf6a4cd65c259fa604d7
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-16 09:08:45 +0530

    Found modified or deleted file,  app/(backend)/api/courses/classrooms/all-classrooms/route.js. Committing Via Global Bash Script:git-status-automated.bat

--- app/(backend)/api/courses/classrooms/all-classrooms/route.js
+++ app/(backend)/api/courses/classrooms/all-classrooms/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import Course from "../../../modules/entities/Course";
 
 /**
  * API Route to get all classrooms of a course
@@ -13,23 +14,26 @@ export async function POST(request) {
       requestData.course_id
     );
 
-    const createClassroomStatus = await Mentor.createClassroom(requestData);
+    const getClassroomStatus = await Course.fetchClassrooms(requestData.id);
 
-    if (createClassroomStatus.success) {
-      console.log("Successfully created new classroom");
+    if (getClassroomStatus.success) {
+      console.log(
+        "Successfully fetched classrooms of course ",
+        requestData.course_id
+      );
       return NextResponse.json(
         {
           success: true,
-          data: createClassroomStatus.data,
+          data: getClassroomStatus.data,
         },
         { status: 200 }
       );
     } else {
-      console.log("Error while creating new classroom");
+      console.log("Error while fetching  classrooms");
       return NextResponse.json(
         {
           success: false,
-          error: createClassroomStatus.error,
+          error: getClassroomStatus.error,
         },
         { status: 400 }
       );

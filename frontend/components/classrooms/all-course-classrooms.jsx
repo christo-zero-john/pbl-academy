d9546af7c7a379d850d2d844f221dda85fc58c25
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-16 22:21:22 +0530

    Found modified or deleted file,  frontend/components/classrooms/all-course-classrooms.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/classrooms/all-course-classrooms.jsx
+++ frontend/components/classrooms/all-course-classrooms.jsx
@@ -1,32 +1,104 @@
+import Course from "@/frontend/modules/entities/Course";
+import Classroom from "@/frontend/modules/entities/Classroom";
+import { useState, useEffect } from "react";
 import { Offcanvas, Accordion } from "react-bootstrap";
+import CreateClassroomForm from "../forms/create-classroom-form";
 
-export default function AllCourseClassrooms({ courseID, show, setShow }) {
-  return (
-    <>
+export default function AllCourseClassrooms({ course, show, setShow }) {
+  console.log("Rendering Classrroms of course ", course.id);
+  const [classrooms, setClassrooms] = useState(null);
+
+  // This useEffect fetches the classrooms and sets classrooms state
+  useEffect(() => {
+    /**
+     *  The below is a Immediately Invoked Function Expression which is an async function to fetch classrooms from supabase. It is used as we can't directly use await statement inside  useEffect.
+     */
+
+    (async () => {
+      console.log("Fetching classrooms");
+      const getClassroomStatus = await Course.fetchClassrooms(course.id);
+
+      if (getClassroomStatus.success) {
+        console.log("Successfully fetched course classrooms");
+        // We should optimize classrooms to be used in the component
+        const optimizedClassrooms = Classroom.optimizeClassroomItems(
+          getClassroomStatus.classrooms,
+          course
+        );
+        setClassrooms(optimizedClassrooms);
+      } else {
+        console.log("Error fetching course classrooms");
+        window.confirm(
+          `Could'nt fetch classrooms: ${getClassroomStatus.error.message}`
+        );
+      }
+    })();
+  }, []);
+
+  if (!classrooms) {
+    return (
       <Offcanvas
         show={show}
         onHide={() => setShow(false)}
         backdrop="static"
         className="w-100"
       >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>All Classrooms</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Accordion defaultActiveKey="null" flush>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Accordion 1</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum,
-                maxime! Animi sed laborum nihil harum quibusdam magnam
-                cupiditate officia nesciunt dolorem molestiae optio consectetur
-                rem veritatis quaerat adipisci debitis fuga sit blanditiis unde,
-                expedita accusantium?
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Offcanvas.Body>
+        Fetching Classrooms...
       </Offcanvas>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <Offcanvas
+          show={show}
+          onHide={() => setShow(false)}
+          backdrop="static"
+          className="w-100"
+        >
+          <Offcanvas.Header
+            closeButton
+            className="border-bottom border-3 border-warning"
+          >
+            <Offcanvas.Title className="text-center w-100">
+              <span className="fs-6 d-block">All Classrooms of</span>
+              <span className="fs-4 text-success d-block">{course.title}</span>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            {
+              // Displaying classrom items of course
+              classrooms.map((classroom, index) => (
+                <div
+                  className="card p-2 m-2 d-flex flex-row justify-content-between align-items-center"
+                  key={index}
+                >
+                  <div className="w-fit">
+                    <span className="d-block  fs-6">Cohort from</span>
+                    <span className="d-block text-success fw-600">
+                      {classroom.start_date.split("-").reverse().join("-")} to{" "}
+                      {classroom.end_date.split("-").reverse().join("-")}
+                    </span>
+                    <span className="fw-700">
+                      Seats: {classroom.total_seats}
+                    </span>
+                    <span className="d-block">
+                      Led By{" "}
+                      <span className="text-success fw-500">
+                        {classroom.created_by}
+                      </span>
+                    </span>
+                  </div>
+                  <button className="btn btn-primary m-2">
+                    Enroll{" "}
+                    <span className="">₹{classroom.enrollment_price}</span>{" "}
+                  </button>
+                </div>
+              ))
+            }
+          </Offcanvas.Body>
+        </Offcanvas>
+        <CreateClassroomForm />
+      </>
+    );
+  }
 }

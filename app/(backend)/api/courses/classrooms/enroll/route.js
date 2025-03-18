1be32f9924d2a7cad4c4032d9a771c3e3744175c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-18 15:36:35 +0530

    Found new file,  app/(backend)/api/courses/classrooms/enroll/route.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/(backend)/api/courses/classrooms/enroll/route.js
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import Course from "../../../modules/entities/Course";
+
+/**
+ * API Route to get all classrooms of a course
+ */
+export async function POST(request) {
+  try {
+    const requestData = await request.json();
+    console.log("Recieved Data: ", requestData);
+
+    console.log(
+      "Initializing enroll learner to classroom ",
+      requestData.classroom_id
+    );
+
+    const enrollmentStatus = await Course.enrollToClassroom(requestData);
+
+    if (enrollmentStatus.success) {
+      console.log(
+        "Successfully fetched classrooms of course ",
+        requestData.course_id
+      );
+      return NextResponse.json(
+        {
+          success: true,
+          classrooms: enrollmentStatus.data,
+        },
+        { status: 200 }
+      );
+    } else {
+      console.log("Error while fetching  classrooms");
+      return NextResponse.json(
+        {
+          success: false,
+          error: enrollmentStatus.error,
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.log("Internal Server Error: ", error);
+    return NextResponse.json(
+      {
+        success: false,
+        error: {
+          message:
+            `Internal Server Error: ${error.message}` ||
+            "Something went wrong. Internal Server Error. Please Contact Support",
+        },
+      },
+      { status: 500 }
+    );
+  }
+}

408057821b8b1a25067f7bf44da8a6aac92f8ebb
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-19 09:24:13 +0530

    Found modified or deleted file,  app/(backend)/api/modules/entities/Classroom.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ app/(backend)/api/modules/entities/Classroom.js
@@ -0,0 +1,49 @@
+import { createClient } from "./Supabase";
+
+/**
+ * Class to handle course related functionalities in backend
+ */
+class Classroom {
+  constructor() {
+    if (!Classroom.instance) {
+      Classroom.instance = this;
+    }
+    return Classroom.instance;
+  }
+
+  async enrollToClassroom(enrollmentData) {
+    try {
+      const Supabase = await createClient();
+      const userSession = await Supabase.auth.getSession();
+      const { data, error } = await Supabase.from("enrollments")
+        .insert(enrollmentData)
+        .select("*");
+      if (error) {
+        console.log("\n\n\nError\n\n\n");
+        return {
+          success: false,
+          error: error,
+        };
+      } else {
+        return {
+          success: true,
+          data: data,
+        };
+      }
+    } catch (error) {
+      // Catch unexpected errors
+      console.log("Internal Server Error: ", error);
+      return {
+        success: false,
+        error: {
+          message:
+            `Internal Server Error: ${error.message}` ||
+            "Something went wrong. Internal Server Error. Please Contact Support",
+        },
+      };
+    }
+  }
+}
+
+export default new Classroom();
+// Classroom in backend

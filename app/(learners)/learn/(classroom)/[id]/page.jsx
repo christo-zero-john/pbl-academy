0c55137c52d28400397e2cabca4ca161618cb9fd
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-20 20:35:03 +0530

    Update page.jsx

--- app/(learners)/learn/(classroom)/[id]/page.jsx
+++ app/(learners)/learn/(classroom)/[id]/page.jsx
@@ -1,3 +1,12 @@
+"use client";
+import { useParams } from "next/navigation";
+
+/**
+ * Displays course classroom
+ */
 export default function ClassroomPage() {
+  const params = useParams();
+  const classroomID = params.id;
+  console.log(classroomID);
   return <div>ClassroomPage</div>;
 }

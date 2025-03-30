8b57a62f406c2c76e702d64ddf6f6fcd84fc11dd
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-30 20:36:54 +0530

    Update Tasks.js

--- frontend/modules/entities/Tasks.js
+++ frontend/modules/entities/Tasks.js
@@ -11,11 +11,12 @@ class Tasks {
   }
 
   /** comment -
+   * Used to Group Tasks into days and sort them by task.index
    * Initially tasks is an array of tasks. We need to convert it into a 2D array of tasks grouped by day, then sort each day's task by 'task.index'. The converted tasks is a 2D array. Each row in the 2D array is a day, and each column in the 2D array is a task of that day.
    * @returns tasks2D: A 2D array of tasks
    */
   groupAndSortTasks(tasks) {
-    console.log("Grouping Tasks into days and sorting by task.index");
+    console.log("Grouping and sorting tasks");
     // Group tasks by day using an object
     const tasksByDay = tasks.reduce((groups, task) => {
       if (!groups[task.day]) {

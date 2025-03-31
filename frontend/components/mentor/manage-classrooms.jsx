05189f41b8178acbc5915d355258cc75a03df66c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-31 21:33:39 +0530

    Found modified or deleted file,  frontend/components/mentor/manage-classrooms.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/mentor/manage-classrooms.jsx
+++ frontend/components/mentor/manage-classrooms.jsx
@@ -1,5 +1,12 @@
-export default function ManageClassrooms({ show, setShow, course }) {
+import { Offcanvas } from "react-bootstrap";
+import AllCourseClassrooms from "../classrooms/all-course-classrooms";
+import ManageClassroomItem__Mentor from "./manage-classroom-item";
+
+export default function ManageClassrooms__Mentor({ course }) {
   return (
-    <p className="">mdkamndskw</p>
+    <AllCourseClassrooms
+      course={course}
+      mentor={true}
+    />
   );
 }

387848f2ca40594b404a51e74cc6568f99754234
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-31 21:33:39 +0530

    Found modified or deleted file,  frontend/components/mentor/mentor-actions.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/mentor/mentor-actions.jsx
+++ frontend/components/mentor/mentor-actions.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Offcanvas } from "react-bootstrap";
 import PublishedCourse__Mentor from "./actions__published-course";
 import UnPublishedCourse__Mentor from "./actions__unpublished-course";
@@ -7,48 +7,6 @@ import UnPublishedCourse__Mentor from "./actions__unpublished-course";
 export default function MentorActions({ course, setCourse }) {
   const [show, setShow] = useState(false);
   const [offcanvasBody, setOffcanvasBody] = useState(<></>);
-
-  useEffect(() => {
-    setDefaultOffcanvasBody();
-  }, []);
-
-  // Default Offcanvas body
-  function setDefaultOffcanvasBody() {
-    setOffcanvasBody(
-      <>
-        <Link
-          href={`/courses/mentor/edit-course/${course.id}`}
-          className="btn btn-primary mx-2"
-        >
-          Edit Course Details
-        </Link>
-
-        <Link
-          href={`/courses/mentor/edit-course/${course.id}/tasks`}
-          className="btn btn-primary mx-2"
-        >
-          Manage Tasks
-        </Link>
-
-        {course.published ? (
-          <PublishedCourse__Mentor
-            course={course}
-            setCourse={setCourse}
-            offcanvasBody={offcanvasBody}
-            setOffcanvasBody={setOffcanvasBody}
-          />
-        ) : (
-          <UnPublishedCourse__Mentor
-            course={course}
-            setCourse={setCourse}
-            offcanvasBody={offcanvasBody}
-            setOffcanvasBody={setOffcanvasBody}
-            setPreviousOffCanvasBody={setDefaultOffcanvasBody}
-          />
-        )}
-      </>
-    );
-  }
 
   return (
     <div className="">
@@ -70,10 +28,25 @@ export default function MentorActions({ course, setCourse }) {
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          {
-            // A useState variable used to set offcanvas bofy as it requires to frequently update offcanvas body in other components
-            offcanvasBody
-          }
+          <Link
+            href={`/courses/mentor/edit-course/${course.id}`}
+            className="btn btn-primary mx-2"
+          >
+            Edit Course Details
+          </Link>
+
+          <Link
+            href={`/courses/mentor/edit-course/${course.id}/tasks`}
+            className="btn btn-primary mx-2"
+          >
+            Manage Tasks
+          </Link>
+
+          {course.published ? (
+            <PublishedCourse__Mentor course={course} setCourse={setCourse} />
+          ) : (
+            <UnPublishedCourse__Mentor course={course} setCourse={setCourse} />
+          )}
         </Offcanvas.Body>
       </Offcanvas>
     </div>

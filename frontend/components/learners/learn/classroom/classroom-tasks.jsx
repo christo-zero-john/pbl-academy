3597775284063a24d7718df52f26a8c1369e7415
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-06 20:52:41 +0530

    Found modified or deleted file,  frontend/components/learners/learn/classroom/classroom-tasks.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/learners/learn/classroom/classroom-tasks.jsx
+++ frontend/components/learners/learn/classroom/classroom-tasks.jsx
@@ -1,7 +1,9 @@
 import Classroom from "@/frontend/modules/entities/Classroom";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function ClassroomTasks({ classroomID, courseID, }) {
+export default function ClassroomTasks({ classroomID, courseID }) {
+  const router = useRouter();
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -12,8 +14,36 @@ export default function ClassroomTasks({ classroomID, courseID, }) {
       );
       console.log(getTasksStatus);
 
-      if(!getTasksStatus.success) {
-        
+      if (getTasksStatus.success) {
+        // console.log("Response recieved");
+        if (getTasksStatus.course.length == 0) {
+          console.log(
+            "Invalid Course. Either course is deleted or course does not exists"
+          );
+          {
+            router.push("/courses/404");
+          }
+        } else if (getTasksStatus.course.length > 1) {
+          window.confirm(
+            "Something unexpected happened while fetching course. Instead of one, we found multiple courses! Contact Support to resolve this problem"
+          );
+        } else {
+          // console.log("Course fetched Successfully");
+          let tempCourse = getTasksStatus.courses[0];
+          tempCourse.tasks = Tasks.groupAndSortTasks(tempCourse.tasks);
+          setCourse(tempCourse);
+        }
+      } else if (getTasksStatus.error) {
+        if (getTasksStatus.error.includes("fetch failed")) {
+          window.confirm(
+            "Failed to connect with database. Check your network connection and try again later..."
+          );
+        } else {
+          window.confirm(
+            "Some Unexpected error while fetching course." +
+              getTasksStatus.error
+          );
+        }
       }
     })();
   });

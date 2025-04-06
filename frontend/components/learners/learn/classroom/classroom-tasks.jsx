f45c4ceb543d427f9fa9e9c0bfada561097cc8c3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-06 20:40:51 +0530

    Found modified or deleted file,  frontend/components/learners/learn/classroom/classroom-tasks.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/learners/learn/classroom/classroom-tasks.jsx
+++ frontend/components/learners/learn/classroom/classroom-tasks.jsx
@@ -1,3 +1,14 @@
-export default function ClassroomTasks() {
+import Classroom from "@/frontend/modules/entities/Classroom";
+
+export default async function ClassroomTasks({ classroomID, courseID }) {
+  const [tasks, setTasks] = useState([]);
+
+  console.log(classroomID, courseID, courseTitle);
+  const getTasksStatus = await Classroom.getClassroomTasks(
+    classroomID,
+    courseID
+  );
+  console.log(getTasksStatus);
+
   return <div>ClassroomTasks</div>;
 }

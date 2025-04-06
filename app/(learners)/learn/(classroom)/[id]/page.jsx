ec6a60ba75a83f97bb64a8b3dddcb1ed88c0c1e2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-06 09:18:16 +0530

    Found modified or deleted file,  app/(learners)/learn/(classroom)/[id]/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/(learners)/learn/(classroom)/[id]/page.jsx
+++ app/(learners)/learn/(classroom)/[id]/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 /**
  * Displays course classroom
@@ -8,5 +9,21 @@ export default function ClassroomPage() {
   const params = useParams();
   const classroomID = params.id;
   console.log(classroomID);
-  return <div>ClassroomPage</div>;
+
+  useEffect(() => {
+    /**
+     * Fetch classroom and course details
+     * Extrac, group and sort tasks
+     * Display tasks daywise along with mark as completed buton.
+     * When clicked on a task, display task details in an offcanvas
+     */
+  });
+
+  return (
+    <div>
+      <h1 className="">
+        Classroom <span className="text-success">Course Name</span>
+      </h1>
+    </div>
+  );
 }

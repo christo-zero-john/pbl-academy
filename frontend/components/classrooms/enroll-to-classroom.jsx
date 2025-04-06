f826d23e9a50a2610155f369f13b0e6ac9a9e5f9
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-06 20:21:37 +0530

    Found modified or deleted file,  frontend/components/classrooms/enroll-to-classroom.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/classrooms/enroll-to-classroom.jsx
+++ frontend/components/classrooms/enroll-to-classroom.jsx
@@ -2,12 +2,9 @@ import Classroom from "@/frontend/modules/entities/Classroom";
 import User from "@/frontend/modules/entities/User";
 import { useRouter } from "next/navigation";
 
-export default function EnrollToClassroom({ classroom, courseTitle }) {
-  courseTitle = courseTitle.split(" ").join("-").toLowerCase();
-  console.log(courseTitle);
-
+export default function EnrollToClassroom({ classroom, course }) {
   const router = useRouter();
-  async function enrollTOClassroomHandler() {
+  async function enrollToClassroomHandler() {
     console.log("Enrolling to classroom");
     const confirmAction = window.confirm(
       "Are you sure you want to enroll to this classroom?"
@@ -23,8 +20,15 @@ export default function EnrollToClassroom({ classroom, courseTitle }) {
       );
 
       if (enrollmentStatus.success) {
+        /**
+         * After the user is enrolled then redirect to course classroom. Classroom link has 3 parts classroom_id, course_id, and course_title. coure title is splitted, joined and converted to lowercase
+         */
         window.confirm("Successfully enrolled to classroom");
-        router.push(`/learn/${enrollmentStatus.classrooms.classroom_id}`);
+        router.push(
+          `/learn/${enrollmentStatus.classrooms.classroom_id}-${
+            course.id
+          }-${course.title.split(" ").join("-").toLowerCase()}`
+        );
       } else {
         window.confirm(
           `Some problem occurred while enrolling: ${enrollmentStatus.error.message} `
@@ -36,7 +40,7 @@ export default function EnrollToClassroom({ classroom, courseTitle }) {
   }
 
   return (
-    <button className="btn btn-primary m-2" onClick={enrollTOClassroomHandler}>
+    <button className="btn btn-primary m-2" onClick={enrollToClassroomHandler}>
       Enroll <span className="">₹{classroom.enrollment_price}</span>{" "}
     </button>
   );

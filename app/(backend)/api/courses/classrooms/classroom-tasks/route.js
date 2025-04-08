6cef9f0073e5413d178555ff3d098a90fe649238
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-08 20:52:47 +0530

    Found modified or deleted file,  app/(backend)/api/courses/classrooms/classroom-tasks/route.js. Committing Via Global Bash Script:git-status-automated.bat

--- app/(backend)/api/courses/classrooms/classroom-tasks/route.js
+++ app/(backend)/api/courses/classrooms/classroom-tasks/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import Classroom from "../../../modules/entities/Classroom";
+import User from "../../../modules/entities/User";
 
 /**
  * API Route to get all tasks of classroom of a course
@@ -14,28 +15,49 @@ export async function POST(request) {
       requestData.classroom_id
     );
 
-    const getTasksStatus = await Classroom.fetchClassroomTasks(
-      requestData
+    let learner_id = requestData.learner_id;
+
+    if (!requestData.learner_id) {
+      const getUserStatus = await User.getUser();
+      if (getUserStatus.success) {
+        learner_id = getUserStatus.data.id;
+      }
+    }
+
+    const getCourseStatus = await Classroom.fetchClassroomTasks(requestData);
+
+    const getCompletedTasksStatus = await Classroom.fecthCompletedTasks(
+      requestData.classroom_id,
+      learner_id
     );
 
-    if (getTasksStatus.success) {
-      console.log(
-        "Successfully fetched classrooms of course ",
-        requestData.classroom_id
-      );
-      return NextResponse.json(
-        {
-          success: true,
-          course: getTasksStatus.data,
-        },
-        { status: 200 }
-      );
+    if (getCourseStatus.success) {
+      if (getCompletedTasksStatus.success) {
+        console.log(
+          `Successfully fetched course ${requestData.course_id} and completed tasks of classroom ${requestData.classroom_id}`
+        );
+        return NextResponse.json(
+          {
+            success: true,
+            course: getCourseStatus.data,
+            completed_tasks: getCompletedTasksStatus.data,
+          },
+          { status: 200 }
+        );
+      } else {
+        console.log(
+          `Successfully fetched course ${requestData.course_id} but failed to fetch tasks of classroom ${requestData.classroom_id}`
+        );
+        throw new Error(
+          `Error fetching tasks of classroom ${requestData.classroom_id}`
+        );
+      }
     } else {
-      console.log("Error while fetching  classrooms");
+      console.log("Error while fetching  classrooms. ");
       return NextResponse.json(
         {
           success: false,
-          error: getTasksStatus.error,
+          error: getCourseStatus.error,
         },
         { status: 400 }
       );

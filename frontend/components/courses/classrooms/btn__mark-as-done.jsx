a37aa65ecca007342bc67747dc56e5ae8cf3424f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-09 21:45:04 +0530

    Update btn__mark-as-done.jsx

--- frontend/components/courses/classrooms/btn__mark-as-done.jsx
+++ frontend/components/courses/classrooms/btn__mark-as-done.jsx
@@ -1,7 +1,19 @@
-export default function MarkAsDoneBtn({ currentTask }) {
+import Classroom from "@/frontend/modules/entities/Classroom";
+import { useParams } from "next/navigation";
+
+export default function MarkAsDoneBtn({
+  currentTask,
+  completedTasks,
+  setCompletedTasks,
+}) {
+  const params = useParams();
+  // URL paramters has such a parameter. Refer file: /learn/(classroom)[id]
+
+  const classroomID = params.id.split("-")[0];
   function markAsDoneHandler() {
     window.confirm("Are you sure you want to mark this task as done?");
-    console.log(currentTask);
+
+    const markAsDoneStatus = Classroom.markTaskAsDone(currentTask, classroomID);
   }
   return (
     <button className="btn btn-success" onClick={markAsDoneHandler}>

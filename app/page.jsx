2ae91951de9ac9a0d919f3ada39d3c74a4ab521c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-14 18:18:14 +0530

    Found modified or deleted file,  app/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Loading from "@/frontend/components/common/loading";
 import User from "@/frontend/modules/entities/User";
 import Link from "next/link";
 
@@ -5,58 +6,8 @@ export default function HomePage() {
   console.log(User.user);
   return (
     <>
-      <h1 className="text-center text-primary fs-3">All Paths in the App</h1>
-      <h2 className="text-center text-success fs-4">Authentication</h2>
-      {User.user ? (
-        <p
-          className="d-block m-1 btn btn-primary w-fit"
-          onClick={() => {
-            User.logout();
-            window.location.reload();
-          }}
-        >
-          Logout
-        </p>
-      ) : (
-        <Link href="/auth/login" className="d-block m-1 btn btn-primary w-fit">
-          Login
-        </Link>
-      )}
-      <br />
-      <Link className="d-block m-1" href="/auth/signup">
-        Signup
-      </Link>{" "}
-      <br />
-      <Link className="d-block m-1 btn btn-primary w-fit" href="/dashboard">
-        User Dashboard
-      </Link>
-      <br />
-      <hr className="border border-3" />
-      <h2 className="text-center text-success fs-4">Courses</h2>
-      <Link className="d-block m-1 btn btn-primary w-fit" href="/courses">
-        All Courses
-      </Link>
-      <br />
-      <Link className="d-block m-1" href="/courses/course-id">
-        Display Course By ID
-      </Link>
-      <br />
-      <Link className="d-block m-1" href="/courses/404">
-        Course Not Found
-      </Link>
-      <br />
-      <Link className="d-block m-1" href="/courses/mentor/">
-        Mentor Dashboard
-      </Link>
-      <br />
-      <Link className="d-block m-1" href="/courses/mentor/all-courses">
-        All Courses of a Mentor
-      </Link>
-      <br />
-      <Link className="d-block m-1" href="/courses/mentor/create-course">
-        Create New Course
-      </Link>
-      <br />
+      <Loading show={true}/>
+      
     </>
   );
 }

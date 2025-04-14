f2eff73d4519b59ced8bec7a329ce97d6c9bbd99
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-14 18:44:49 +0530

    Found modified or deleted file,  app/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,14 @@ import Loading from "@/frontend/components/common/loading";
 import NavBar from "@/frontend/components/common/nav-bar";
 import HeroSection from "@/frontend/components/routes/home-page/hero-section";
 import User from "@/frontend/modules/entities/User";
-import Link from "next/link";
+import "../frontend/styles/home-page.css";
 
 export default function HomePage() {
   console.log(User.user);
   return (
     <>
       <Loading show={false} />
-      <NavBar active="home"/>
+      <NavBar active="home" />
       <HeroSection />
     </>
   );

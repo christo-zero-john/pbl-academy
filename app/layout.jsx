1e34d0a5c48ba8e1d925012f6a3360b802fe07a5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-14 18:58:52 +0530

    Found modified or deleted file,  app/layout.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,6 +15,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Bootstrap />
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+        />
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+      </head>
       <body>{children}</body>
     </html>
   );

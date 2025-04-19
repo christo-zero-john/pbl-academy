33afbcf5bca3b8e796bb5b03f3a49a7f0ca67b90
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-19 09:24:08 +0530

    Update form-container.jsx

--- frontend/components/routes/auth/form-container.jsx
+++ frontend/components/routes/auth/form-container.jsx
@@ -1,14 +1,63 @@
-export default function FormContainer() {
-  return (
-    <div class="form-container sign-up-container ">
-      <form action="#">
+export default function FormContainer({
+  varient,
+  handler = null,
+  formData = null,
+  setFormData = null,
+}) {
+  const varients = {
+    create_account: (
+      <>
         <h1>Create Account</h1>
-        <span>Using your email ID</span>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Sign Up</button>
-      </form>
+        <span>Using your email ID</span>        
+        <input
+          type="email"
+          placeholder="Email"
+          value={formData?.email || ""}
+          onChange={(e) =>
+            setFormData?.({ ...formData, email: e.target.value })
+          }
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={formData?.password || ""}
+          onChange={(e) =>
+            setFormData?.({ ...formData, password: e.target.value })
+          }
+        />
+        <button type="submit">Create Account</button>
+      </>
+    ),
+    login: (
+      <>
+        <h1>Log in</h1>
+        <span>to your account</span>
+        <input
+          type="email"
+          placeholder="Email"
+          value={formData?.email || ""}
+          onChange={(e) =>
+            setFormData?.({ ...formData, email: e.target.value })
+          }
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={formData?.password || ""}
+          onChange={(e) =>
+            setFormData?.({ ...formData, password: e.target.value })
+          }
+        />
+        <a href="#">Forgot your password?</a>
+        <button type="submit">Log In</button>
+      </>
+    ),
+  };
+  console.log(varients[varient]);
+
+  return (
+    <div className={`form-container ${varient === 'login' ? 'sign-in-container' : 'sign-up-container'}`}>
+      <form onSubmit={handler}>{varients[varient]}</form>
     </div>
   );
 }

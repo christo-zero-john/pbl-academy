4de10d80d56730f2df70d17f105fa08ba016d5a3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-20 09:49:43 +0530

    Found modified or deleted file,  app/(backend)/api/auth/login/route.js. Committing Via Global Bash Script:git-status-automated.bat

--- app/(backend)/api/auth/login/route.js
+++ app/(backend)/api/auth/login/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
     );
 
     const loginStatus = await User.login(email, password);
-    // console.log(loginStatus);
+    console.log(loginStatus);
 
     if (!loginStatus.success) {
       return NextResponse.json(

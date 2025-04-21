7938ee503dab445da0c06c68423fe3d6a5a47edb
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-21 11:34:25 +0530

    Found modified or deleted file,  frontend/components/courses/classrooms/classroom-item.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/courses/classrooms/classroom-item.jsx
+++ frontend/components/courses/classrooms/classroom-item.jsx
@@ -1,3 +1,31 @@
 export default function ClassroomItem() {
-  return <div>CourseItem</div>;
+  return (
+    <div class="col-lg-4 col-md-6 mb-4 border rounded shadow-lg">
+      <div class="rounded overflow-hidden mb-2">
+        <img class="img-fluid" src="img/course-1.jpg" alt="" />
+        <div class="p-4">
+          <div class="d-flex justify-content-between mb-3">
+            <small class="m-0">
+              <i class="fa fa-users text-primary mr-2"></i>25 Students
+            </small>
+            <small class="m-0">
+              <i class="far fa-clock text-primary mr-2"></i>01h 30m
+            </small>
+          </div>
+          <a class="h5" href="">
+            Web design & development courses for beginner
+          </a>
+          <div class="border-top mt-4 pt-4">
+            <div class="d-flex justify-content-between">
+              <h6 class="m-0">
+                <i class="fa fa-star text-primary mr-2"></i>4.5{" "}
+                <small>(250)</small>
+              </h6>
+              <h5 class="m-0">$99</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

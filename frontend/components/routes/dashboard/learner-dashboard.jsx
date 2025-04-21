e10c3548c1544468ebe8df5844e4a81a06c4b4cf
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-21 11:26:57 +0530

    Found modified or deleted file,  frontend/components/routes/dashboard/learner-dashboard.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/routes/dashboard/learner-dashboard.jsx
+++ frontend/components/routes/dashboard/learner-dashboard.jsx
@@ -2,6 +2,8 @@ export default function LearnerDashboard() {
   return (
     <div>
       <h1 className="text-center">Welcome Learner</h1>
+      <p className="fw-600 fs-3">Classrooms</p>
+      
     </div>
   );
 }

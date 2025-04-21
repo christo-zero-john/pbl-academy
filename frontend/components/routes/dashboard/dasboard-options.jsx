3d7d284b9a07c6a3aa0ec8d61a0a8445bfd749b3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-21 10:46:13 +0530

    Found modified or deleted file,  frontend/components/routes/dashboard/dasboard-options.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/routes/dashboard/dasboard-options.jsx
+++ frontend/components/routes/dashboard/dasboard-options.jsx
@@ -1,10 +1,28 @@
-import Link from "next/link";
-export default function DasboardOptions() {
+export default function DasboardOptions({ setOption }) {
   return (
-    <>
-      <Link href="/mentor/dashboard">Mentor Dashboard</Link>
-      <Link href="/dashboard">Learner Dashboard</Link>
-      <Link href="/admin/dashboard">Admin Dashboard</Link>
-    </>
+    <div className="flex flex-col gap-4 p-4">
+      <h1 className="text-center">Goto Dashboard</h1>
+      <p
+        onClick={setOption("mentor")}
+        className="d-block w-fit p-3 m-2 mx-auto rounded nav-link link-light bg-primary"
+        href="/mentor/dashboard"
+      >
+        Mentor Dashboard
+      </p>
+      <p
+        onClick={setOption("learner")}
+        className="d-block w-fit p-3 m-2 mx-auto rounded nav-link link-light bg-success"
+        href="/dashboard"
+      >
+        Learner Dashboard
+      </p>
+      <p
+        onClick={setOption("admin")}
+        className="d-block w-fit p-3 m-2 mx-auto rounded nav-link link-danger bg-warning"
+        href="/admin/dashboard"
+      >
+        Admin Dashboard
+      </p>
+    </div>
   );
 }

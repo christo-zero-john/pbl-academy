a40882ceebb3adcecc38475487d2518be2d936d2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-21 14:15:48 +0530

    Found modified or deleted file,  frontend/modules/entities/Enrollments.js. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/modules/entities/Enrollments.js
+++ frontend/modules/entities/Enrollments.js
@@ -10,9 +10,39 @@ class Enrollments {
     return Enrollments.instance;
   }
 
-  
+  async getEnrollmentsOfLearner(learner_id) {
+    console.log(
+      "Sending request to  get all classrooms of learner: ",
+      learner_id
+    );
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ learner_id: learner_id }),
+    };
 
+    console.log("Request: ", request);
 
+    return fetch("/api/courses/classrooms/my-classrooms", request)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("Request sent successfully and response recieved.");
+        console.log(response);
+        if (response.error) {
+          return {
+            success: false,
+            error: response.error,
+          };
+        } else {
+          return {
+            success: true,
+            data: response?.data,
+          };
+        }
+      });
+  }
 }
 
 export default new Enrollments();

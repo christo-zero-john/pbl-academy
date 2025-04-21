68ad5aaa372424a328ea89062dc2af22a62e778c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-21 11:17:49 +0530

    Found modified or deleted file,  frontend/components/routes/dashboard/mentor-dashboard.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/routes/dashboard/mentor-dashboard.jsx
+++ frontend/components/routes/dashboard/mentor-dashboard.jsx
@@ -1,3 +1,11 @@
-export default function MentorDashboard() {
-  return <div>MentorDashboard</div>;
+import Link from "next/link";
+
+export default function MentorDasboard() {
+  return (
+    <div>
+      <h1 className="text-center">Welcome Mentor</h1>
+      <Link href="/courses/mentor/all-courses">All Courses</Link> <br />
+      <Link href="/courses/mentor/create-course">New Course</Link>
+    </div>
+  );
 }

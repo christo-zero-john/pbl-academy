f4268963416e5811d9c75832e26e08c1a7906ed9
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-22 13:46:57 +0530

    Update page.jsx

--- app/(learners)/learn/(classroom)/[id]/page.jsx
+++ app/(learners)/learn/(classroom)/[id]/page.jsx
@@ -1,7 +1,9 @@
 "use client";
+import NavBar from "@/frontend/components/common/nav-bar";
 import ClassroomTasks from "@/frontend/components/learners/learn/classroom/tasks/classroom-tasks";
 import { useParams } from "next/navigation";
 import { useState } from "react";
+import { Navbar } from "react-bootstrap";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 /**
@@ -18,39 +20,45 @@ export default function ClassroomPage() {
   });
 
   return (
-    <div>
-      <h1 className="text-center m-4">
-        <span className="fs-4 d-block">Learn</span>{" "}
-        <span className="text-success">{courseTitle.split("-").join(" ")}</span>
-      </h1>
+    <>
+      <NavBar />
+      <div>
+        <h1 className="text-center m-4">
+          <span className="fs-4 d-block">Learn</span>{" "}
+          <span className="text-success">
+            {courseTitle.split("-").join(" ")}
+          </span>
+        </h1>
 
-      <Tabs
-        defaultActiveKey="tasks"
-        id="justify-tab-example"
-        className="mb-3"
-        justify
-      >
-        <Tab eventKey="tasks" title="Activities">
-          <ClassroomTasks
-            classroomID={classroomID}
-            courseID={courseID}
-            classroom={{
-              classroomData: classroomData,
-              setClassroomData: setClassroomData,
-            }}
-          />
-        </Tab>
-        <Tab eventKey="learners" title="Learners">
-          Tab content for Profile
-        </Tab>
-        <Tab eventKey="longer-tab" title="Group Discussion">
-          Tab content for Group Chat
-        </Tab>
-        <Tab eventKey="contact" title="Details">
-          Tab content for Course deatils
-        </Tab>
-      </Tabs>
-    </div>
+        <Tabs
+          defaultActiveKey="tasks"
+          id="justify-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="tasks" title="Activities">
+            <ClassroomTasks
+              classroomID={classroomID}
+              courseID={courseID}
+              classroom={{
+                classroomData: classroomData,
+                setClassroomData: setClassroomData,
+              }}
+            />{" "}
+          </Tab>
+          {/*
+          <Tab eventKey="learners" title="Learners">
+            Tab content for Profile
+          </Tab>
+          <Tab eventKey="longer-tab" title="Group Discussion">
+            Tab content for Group Chat
+          </Tab>
+          <Tab eventKey="contact" title="Details">
+            Tab content for Course deatils
+          </Tab> */}
+        </Tabs>
+      </div>
+    </>
   );
 }
 

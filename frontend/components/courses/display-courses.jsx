a24be8ad041b1f0dc9344a41159cddd4874744db
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-24 13:48:34 +0530

    Found modified or deleted file,  frontend/components/courses/display-courses.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- frontend/components/courses/display-courses.jsx
+++ frontend/components/courses/display-courses.jsx
@@ -2,19 +2,22 @@ import Link from "next/link";
 import React from "react";
 
 export default function DisplayCourses({ courses }) {
+  console.log(courses);
   return (
     <div>
-      <p className="text-center">{courses.length} courses</p>
+      <p className="text-center m-3 fs-4 fw-500 text-uppercase text-danger">
+        {courses.length} courses
+      </p>
       {courses.map((course) => (
         <div
           key={course.id}
-          className="border border-3 border-secondary p-3 m-2 w-fit d-inline-block m-2"
+          className="border border-3 border-success rounded p-3 m-2 col-12 col-md-3 d-inline-block m-2"
         >
           <div className="">
             <h1 className="fs-5 fw- d-inline-block mx-2">{course.title}</h1>
             <p className="small">
               <span className="fw-bold">By: </span>
-              {course.created_by.id}
+              {course.created_by.first_name} {course.created_by.last_name}
             </p>
           </div>
           <Link

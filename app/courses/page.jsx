daef72a0ab15b84286798756b67b2b7189624ca2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-24 13:48:33 +0530

    Found modified or deleted file,  app/courses/page.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import DisplayCourses from "../../frontend/components/courses/display-courses";
+import NavBar from "@/frontend/components/common/nav-bar";
 
 function AllCourses() {
   const [courses, setCourses] = useState(null);
@@ -27,15 +28,15 @@ function AllCourses() {
     })();
   }, []);
 
-  if (courses == null) {
-    return <div>Fetching Courses...</div>;
-  } else {
-    if (courses.length == 0) {
-      return <div>0 Published Courses Found</div>;
-    } else {
-      return <DisplayCourses courses={courses} />;
-    }
-  }
+  return (
+    <>
+      <NavBar />
+      {(!courses && <div>Fetching Courses...</div>) ||
+        (courses.length == 0 && <div>0 Published Courses Found</div>) || (
+          <DisplayCourses courses={courses} />
+        )}
+    </>
+  );
 }
 
 export default AllCourses;
